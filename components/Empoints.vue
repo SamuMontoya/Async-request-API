07949ae3c76e77c9674dd921ac9040e8b5c7d237
SamuMontoya <samu7montoya@gmail.com>
2022-10-04 17:08:47 -0500

    Form

---
+++ components/Empoints.vue
@@ -0,0 +1,35 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <button @click="divisaPrecio">Divisas con precio</button>
+    <p>{{ precios }}</p>
+    <hr />
+    <button @click="divisaNombres">Nombres Divisas</button>
+    <p>{{ nombres }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+
+  data(){
+    return {
+      precios:null,
+      nombres:null,
+    }
+  },
+  methods: {
+    async divisaPrecio() {
+      const api = 'https://openexchangerates.org/api/latest.json?app_id=0f3e3db7c8bc483dbd8a79775ddc9de6'
+      const response = await this.$axios.get(api)
+      this.precios = response
+    },
+    async divisaNombres() {
+      const api = 'https://openexchangerates.org/api/currencies.json?app_id=0f3e3db7c8bc483dbd8a79775ddc9de6'
+      const response = await this.$axios.get(api)
+      this.nombres = response
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,52 @@
 <template>
-  <Tutorial/>
+  <b-container>
+    <h1>Aplicación de divisas</h1>
+    <div>
+      <b-form>
+        <label for="monto">Monto</label>
+        <b-form-input
+          type="number"
+          min="1"
+          id="monto"
+          v-model="monto"
+        ></b-form-input>
+
+        <label for="input-list">Moneda de Origen</label>
+        <b-form-input
+          v-model="origen"
+          list="input-list"
+          id="input-with-list"
+        ></b-form-input>
+        <b-form-datalist id="input-list" :options="options"></b-form-datalist>
+
+        <label for="input-list-2">Moneda de Objetivo</label>
+        <b-form-input
+          v-model="objetivo"
+          list="input-list-2"
+          id="input-with-list-2"
+        ></b-form-input>
+        <b-form-datalist id="input-list-2" :options="options"></b-form-datalist>
+
+        <b-button class="mt-3" variant="primary" @click="submitForm">Submit</b-button>
+      </b-form>
+    </div>
+  </b-container>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  
+  data() {
+    return {
+      monto: null,
+      origen: null,
+      objetivo: null,
+      options: ["1", "2", "3", "4", "5"],
+    };
+  },
+  methods: {
+
+  }
+};
 </script>
